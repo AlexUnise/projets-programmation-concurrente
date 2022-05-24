0a7b7dba18afd37de854d7328cabeea00fc0d996
Alex <alexouenadio2@gmail.com>
2022-05-24 11:50:54 +0200

    hit-miss final

--- hit-miss.py
+++ hit-miss.py
@@ -13,19 +13,23 @@ def frequence_de_hits_pour_n_essais(nb_iteration):
 if __name__ == '__main__':
     compteur = mp.Value('i',0)
     # Nombre d’essai pour l’estimation
-    nb_total_iteration = 10000000
+    nb_total_iteration = 30000000
     #Nombre de processus
-    N = 3
-
+    N = 5
+    time1 = time.time()
     for i in range(N):
         if os.fork() == 0:
             nb_hits=frequence_de_hits_pour_n_essais(nb_total_iteration // N)
-            print(nb_hits)
             with compteur.get_lock():
                 compteur.value += nb_hits
             sys.exit(0)
 
+    for i in range(N):
+        os.wait()
+        
     total_hits = compteur.value   
+    time2 = time.time()
     print("Valeur estimée Pi par la méthode " + str(N) + "−Processus : ", 4 *total_hits / nb_total_iteration)
+    print("Temps de calcul:", time2-time1)
     #TRACE :
     # Calcul Mono−Processus : Valeur estimée Pi par la méthode Mono−Processus : 3.1412604
