20004c5de686043ef9f16d53d554a2284059d220
Alex <alexouenadio2@gmail.com>
2022-05-24 11:30:43 +0200

    hit-miss 1

--- hit-miss.py
+++ hit-miss.py
@@ -1,4 +1,5 @@
-import random, time
+from aifc import Error
+import random, time, os, multiprocessing as mp, sys
 # calculer le nbr de hits dans un cercle unitaire (utilisé par les différentes méthodes)
 def frequence_de_hits_pour_n_essais(nb_iteration):
     count = 0
@@ -8,9 +9,23 @@ def frequence_de_hits_pour_n_essais(nb_iteration):
         # si le point est dans l’unit circle
         if x *x + y *y <= 1: count += 1
     return count
-# Nombre d’essai pour l’estimation
-nb_total_iteration = 10000000
-nb_hits=frequence_de_hits_pour_n_essais(nb_total_iteration)
-print("Valeur estimée Pi par la méthode Mono−Processus : ", 4 *nb_hits / nb_total_iteration)
-#TRACE :
-# Calcul Mono−Processus : Valeur estimée Pi par la méthode Mono−Processus : 3.1412604
+
+if __name__ == '__main__':
+    compteur = mp.Value('i',0)
+    # Nombre d’essai pour l’estimation
+    nb_total_iteration = 10000000
+    #Nombre de processus
+    N = 3
+
+    for i in range(N):
+        if os.fork() == 0:
+            nb_hits=frequence_de_hits_pour_n_essais(nb_total_iteration // N)
+            print(nb_hits)
+            with compteur.get_lock():
+                compteur.value += nb_hits
+            sys.exit(0)
+
+    total_hits = compteur.value   
+    print("Valeur estimée Pi par la méthode " + str(N) + "−Processus : ", 4 *total_hits / nb_total_iteration)
+    #TRACE :
+    # Calcul Mono−Processus : Valeur estimée Pi par la méthode Mono−Processus : 3.1412604
